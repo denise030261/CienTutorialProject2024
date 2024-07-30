929e85471d25e67b3a86859586b3b56af9383b37
denise <denise030261@gmail.com>
2024-07-31 01:49:23 +0900

    자폭 기능 구현

--- CienTutorialProject2024/Assets/Scripts/EnemyAI.cs
+++ CienTutorialProject2024/Assets/Scripts/EnemyAI.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.AI;
 using static UnityEditor.PlayerSettings;
+using Debug = UnityEngine.Debug;
 
 public class EnemyAI : MonoBehaviour
 {
@@ -12,12 +14,13 @@ public class EnemyAI : MonoBehaviour
     float playerDist;
     float noMoveDist=1f;
     bool isTarget;
+    bool isWall = false;
     EnemyBomb enemyBomb = null;
 
     [Range(0f, 360f)][SerializeField] float ViewAngle = 0f;
     [SerializeField] float ViewRadius = 1f;
     LayerMask targetMask;
-    LayerMask obstacleMask;
+    public LayerMask obstacleMask;
 
     Vector3 AngleToDir(float angle)
     {
@@ -27,23 +30,51 @@ public class EnemyAI : MonoBehaviour
 
     private void Start()
     {
+        // 레이어 이름으로 LayerMask 생성
+        int layer = LayerMask.NameToLayer("Wall");
+        if (layer == -1)
+        {
+            //Debug.LogError("Layer 'Wall' not found.");
+            return;
+        }
+
+        // 씬의 모든 게임 오브젝트를 찾음
+        GameObject[] allObjects = FindObjectsOfType<GameObject>();
+        List<GameObject> wallObjects = new List<GameObject>();
+
+        // 레이어를 가진 오브젝트 필터링
+        foreach (GameObject obj in allObjects)
+        {
+            if (obj.layer == layer)
+            {
+                wallObjects.Add(obj);
+            }
+        }
+
+        // Wall 레이어를 가진 모든 오브젝트를 출력
+        foreach (GameObject wallObj in wallObjects)
+        {
+            Debug.Log("Found Wall object: " + wallObj.name);
+        }
+
         nav = GetComponent<NavMeshAgent>();
-        animator=GetComponent<Animator>();
-        enemyBomb=GetComponent<EnemyBomb>();
+        animator = GetComponent<Animator>();
+        enemyBomb = GetComponent<EnemyBomb>();
         target = GameObject.Find("Player");
         targetMask = LayerMask.GetMask("Player");
-        obstacleMask = LayerMask.GetMask("Wall");
+        //obstacleMask = LayerMask.GetMask("Wall"); // 벽 레이어 마스크 설정
     }
 
     private void Update()
     {
+        isTarget = false;
         ViewOfField();
         ChaseTarget();
 
-        DrawDebug();
+        ViewOfFieldDebug();
     }
 
-    void DrawDebug()
+    void ViewOfFieldDebug()
     {
         Vector3 viewAngleA = DirFromAngle(-ViewAngle / 2, false);
         Vector3 viewAngleB = DirFromAngle(ViewAngle / 2, false);
@@ -62,20 +93,32 @@ public class EnemyAI : MonoBehaviour
 
     void ViewOfField()
     {
-        float lookingAngle = transform.eulerAngles.y;  //캐릭터가 바라보는 방향의 각도
+        float lookingAngle = transform.eulerAngles.y;  // 캐릭터가 바라보는 방향의 각도
         Vector3 lookDir = AngleToDir(lookingAngle);
 
         Collider[] Targets = Physics.OverlapSphere(transform.position, ViewRadius, targetMask);
-        // 벽 처리
 
         if (Targets.Length == 0) return;
         foreach (Collider targetCollider in Targets)
         {
             Vector3 targetPos = targetCollider.transform.position;
             Vector3 targetDir = (targetPos - transform.position).normalized;
+            float targetDist = Vector3.Distance(transform.position, targetCollider.gameObject.transform.position);
             float targetAngle = Mathf.Acos(Vector3.Dot(lookDir, targetDir)) * Mathf.Rad2Deg;
-            if (targetAngle <= ViewAngle * 0.5f && !Physics.Raycast(transform.position, targetDir, ViewRadius, obstacleMask))
+
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, targetDir, out hit,targetDist) && hit.collider.tag == "Wall")
             {
+                isWall = true;
+            }
+            else
+            {
+                isWall = false;
+            }
+
+            if (targetAngle <= ViewAngle * 0.5f && !isWall)
+            {
+                Debug.Log("플레이어 발견!");
                 isTarget = true;
             }
         }
@@ -101,6 +144,11 @@ public class EnemyAI : MonoBehaviour
                 enemyBomb.Bomb();
                 isTarget = false;
             }
+        }
+        else if(!isTarget)
+        {
+            nav.SetDestination(transform.position);
+            animator.SetBool("isWalk", false);
         }
     }
 
--- CienTutorialProject2024/Assets/Scripts/EnemyBomb.cs
+++ CienTutorialProject2024/Assets/Scripts/EnemyBomb.cs
@@ -5,17 +5,30 @@ using static UnityEngine.GraphicsBuffer;
 
 public class EnemyBomb : MonoBehaviour
 {
+    [SerializeField] GameObject effectObject;
+    [SerializeField] GameObject visualObject;
+
     bool isBomb;
     Animator animator;
+    List<GameObject> Effects = new List<GameObject>();
+  
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+
+        for (int i = 0; i < effectObject.transform.childCount; i++) 
+        {
+            Effects.Add(effectObject.transform.GetChild(i).gameObject);
+            Effects[i].SetActive(false);
+        }
     }
 
     public void Bomb()
     {
         AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        Effects[0].SetActive(true);
 
         // Bomb 상태가 활성화된 경우
         if (stateInfo.IsName("Bomb"))
@@ -35,7 +48,17 @@ public class EnemyBomb : MonoBehaviour
     void SelfDestructEnd()
     {
         Debug.Log("Bomb");
-        Destroy(gameObject);
+        Effects[0].SetActive(false);
+        Effects[1].SetActive(true);
+        visualObject.SetActive(false);
+        StartCoroutine(BombAnimation());
     }
 
+
+    IEnumerator BombAnimation()
+    {
+        yield return new WaitForSeconds(Effects[1].GetComponent<ParticleSystem>().main.duration+1f);
+        Destroy(gameObject);
+
+    }
 }
