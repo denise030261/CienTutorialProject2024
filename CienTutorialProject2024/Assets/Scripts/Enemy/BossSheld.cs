62626b6a589dcab96d29b988d18c3998890bf25e
denise <denise030261@gmail.com>
2024-08-12 01:59:01 +0900

    보스 보호막 및 적 소환 베타

---
+++ CienTutorialProject2024/Assets/Scripts/Enemy/BossSheld.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSheld : MonoBehaviour
+{
+    [SerializeField] ParticleSystem shieldEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shieldEffect = transform.GetChild(0).GetComponent<ParticleSystem>();
+        Invoke("StopEffect", 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 발사체 없애기
+    }
+
+    void StopEffect()
+    {
+        Debug.Log("방어");
+        shieldEffect.Pause();
+    }
+}
