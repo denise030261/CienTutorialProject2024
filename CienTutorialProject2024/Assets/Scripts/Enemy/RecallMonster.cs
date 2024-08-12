e60f772a5551133bb6d9a3bb35f09c563317280d
denise <denise030261@gmail.com>
2024-08-13 02:11:23 +0900

    적 기믹 구현

--- CienTutorialProject2024/Assets/Scripts/Enemy/RecallMonster.cs
+++ CienTutorialProject2024/Assets/Scripts/Enemy/RecallMonster.cs
@@ -6,20 +6,19 @@ public class RecallMonster : MonoBehaviour
 {
     [SerializeField] float recallTime = 10f;
     [SerializeField] List<GameObject> enemies = new List<GameObject>();
+    [SerializeField] GameObject recallEffect;
+    [SerializeField] GameObject recallingEffect;
     [SerializeField] GameObject recallField;
     Animator animator;
+
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        recallingEffect.SetActive(false);
         Invoke("RecallingMonster", recallTime);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Invoke("OnAnimationStart", recallTime - 1f);
     }
 
     void RecallingMonster()
@@ -30,7 +29,9 @@ public class RecallMonster : MonoBehaviour
                 recallField.transform.position.y + recallField.transform.localScale.y,
                 recallField.transform.position.z + Random.Range(-recallField.transform.localScale.z / 2, recallField.transform.localScale.z / 2));
             animator.SetBool("recallMonster", true);
+            Invoke("OnAnimationEnd", 2.33f);
             Instantiate(enemies[Random.Range(0, 2)], recallPosition, Quaternion.identity);
+            Instantiate(recallEffect, recallPosition, Quaternion.identity);
             Invoke("RecallingMonster", recallTime);
         }
         else
@@ -38,4 +39,17 @@ public class RecallMonster : MonoBehaviour
             Debug.Log("필드를 선정하지 않았습니다");
         }
     }
+
+    public void OnAnimationStart()
+    {
+        animator.SetBool("recallMonster", true);
+        recallingEffect.SetActive(true);
+    }
+
+    public void OnAnimationEnd()
+    {
+        animator.SetBool("recallMonster", false);
+        recallingEffect.SetActive(false);
+    }
+
 }
